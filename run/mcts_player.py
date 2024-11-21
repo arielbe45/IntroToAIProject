683338d89e7bfe1a5edf4339ee9f74f1da47e672
Ariel Ben Eliezer <ariel.ben.eliezer.45@gmail.com>
2024-11-21 16:54:01 +0200

    changed mcts

--- run/mcts_player.py
+++ run/mcts_player.py
@@ -2,9 +2,9 @@ import math
 import random
 from typing import Optional
 
-from run.player import AbstractQuoridorPlayer
 from game.game_state import GameState
 from game.move import Move
+from run.player import AbstractQuoridorPlayer
 
 
 class Node:
@@ -16,8 +16,8 @@ class Node:
         self.wins = 0
         self.children = []
 
-    def is_fully_expanded(self) -> bool:
-        return len(self.children) == len(self.state.get_legal_moves())
+    def is_fully_expanded(self, restrict) -> bool:
+        return len(self.children) == len(self.state.get_legal_moves(restrict=restrict, check_bfs=False))
 
     def best_child(self, exploration_param: float = 1.414) -> 'Node':
         # Uses the UCB1 formula to choose the best child based on exploration-exploitation
@@ -25,9 +25,9 @@ class Node:
             math.log(self.visits) / child.visits))
 
     def expand(self):
-        legal_moves = self.state.get_legal_moves()
+        legal_moves = self.state.get_legal_moves(check_bfs=False)
         for move in legal_moves:
-            next_state = self.state.get_new_state(move)
+            next_state = self.state.get_new_state(move, check_legal=False)
             child_node = Node(state=next_state, parent=self, move=move)
             self.children.append(child_node)
 
@@ -38,13 +38,14 @@ class Node:
 
 class MCTSPlayer(AbstractQuoridorPlayer):
     def __init__(self, heurisric, player, depth, restrict_walls, num_simulations: int = 1000,
-                 exploration_param: float = 1.414):
+                 exploration_param: float = 1.414, rollout_exploration_param: float = 0.7):
         self.heuristic = heurisric
         self.player = player
         self.depth = depth
         self.restrict_walls = restrict_walls
         self.num_simulations = num_simulations
         self.exploration_param = exploration_param
+        self.rollout_exploration_param = rollout_exploration_param
 
     def get_next_move(self, state: GameState) -> Move:
         root = Node(state=state)
@@ -56,11 +57,15 @@ class MCTSPlayer(AbstractQuoridorPlayer):
 
         # Select the move with the most visits as the final choice
         best_child = max(root.children, key=lambda child: child.visits)
-        return best_child.move
+        if state.is_move_legal(move=best_child.move, check_bfs=True):
+            return best_child.move
+
+        return random.choice(state.get_legal_moves(check_bfs=True, restrict=False))
+
 
     def _select(self, node: Node) -> Node:
         while not node.state.is_game_over():
-            if not node.is_fully_expanded():
+            if not node.is_fully_expanded(restrict=self.restrict_walls):
                 return self._expand(node)
             else:
                 node = node.best_child(self.exploration_param)
@@ -68,29 +73,22 @@ class MCTSPlayer(AbstractQuoridorPlayer):
 
     def _expand(self, node: Node) -> Node:
         # Expand a new child from unexplored moves
-        untried_moves = [move for move in node.state.get_legal_moves(self.restrict_walls) if
+        untried_moves = [move for move in node.state.get_legal_moves(restrict=self.restrict_walls, check_bfs=False) if
                          move not in [child.move for child in node.children]]
-        move = random.choice(untried_moves)
-        next_state = node.state.get_new_state(move)
+        move = self._choose_move(state=node.state, possible_moves=untried_moves)
+        next_state = node.state.get_new_state(move, check_legal=False)
         child_node = Node(state=next_state, parent=node, move=move)
         node.children.append(child_node)
         return child_node
 
-    # def _expand(self, node: Node) -> Node:
-    #     # Get legal moves and rank them based on heuristic
-    #     untried_moves = [move for move in node.state.get_legal_moves(self.restrict_walls) if
-    #                      move not in [child.move for child in node.children]]
-    #     if not untried_moves:
-    #         return None
-    #
-    #     # Use heuristic to sort moves and pick the best one to expand
-    #     sorted_moves = sorted(untried_moves, key=lambda move: self.heuristic(node.state.get_new_state(move)),
-    #                           reverse=True)
-    #     move = sorted_moves[0]  # Choose the move with the highest heuristic score
-    #     next_state = node.state.get_new_state(move)
-    #     child_node = Node(state=next_state, parent=node, move=move)
-    #     node.children.append(child_node)
-    #     return child_node
+    def _choose_move(self, state: GameState, possible_moves: list[Move]) -> Move:
+        if random.random() < self.rollout_exploration_param:
+            # choose a random move from restricted moves
+            return random.choice(possible_moves)
+        else:
+            # Choose a move based on heuristics rather than randomly
+            return max(possible_moves, key=lambda move: self.heuristic(state.get_new_state(move, check_legal=False),
+                                                                    player=self.player))
 
     def _simulate(self, state: GameState) -> float:
         current_state = state
@@ -100,11 +98,9 @@ class MCTSPlayer(AbstractQuoridorPlayer):
             if depth >= self.depth:
                 return self.heuristic(current_state, player=self.player)
 
-            # Choose a move based on heuristics rather than randomly
-            legal_moves = current_state.get_legal_moves(self.restrict_walls)
-            move = max(legal_moves,
-                       key=lambda move: self.heuristic(current_state.get_new_state(move), player=self.player), )
-            current_state = current_state.get_new_state(move)
+            legal_moves = state.get_legal_moves(restrict=self.restrict_walls, check_bfs=False)
+            move = self._choose_move(current_state, possible_moves=legal_moves)
+            current_state = current_state.get_new_state(move, check_legal=False)
             depth += 1
 
         # At game end, return binary win/loss
